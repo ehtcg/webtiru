2ae5407a0559f7cb8e146eef168b6d810311f818
pinal <pineal@ehtcgnet.com>
2019-08-24 19:03:49 -0500

    Subtract all

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
 def add(x,y):
     pass
-def substract9x,y):
-    pass
+def substract(x,y):
+    return x-y;
 def multiply(x,y):
-    return x*y
+    return x*y;
 def divide(x,y):
     pass
 def square(x):
